3d2ed05961307a7b6ba42585c2c4050865f70cbd
Alexander Rowland <rowlanda@protonmail.com>
2020-07-24 17:48:37 -0400

    Skip invalid parameters

--- parser.py
+++ parser.py
@@ -5,6 +5,19 @@ path = 'config/example.txt'
 with open(path, 'r') as f:
     data = f.readlines()
 
+valid_parameters = [
+    'host',
+    'server_id',
+    'server_load_alarm',
+    'user',
+    'verbose',
+    'test_mode',
+    'debug_mode',
+    'log_file_path',
+    'send_notifications'
+]
+
+parsed_lines = []
 for line in data:
     # skip commented lines
     if line[:1] == '#':
@@ -12,4 +25,8 @@ for line in data:
     # strip out all whitespace
     trimmed_line = ''.join(line.split())
     parsed_line = trimmed_line.split('=')
-    # print(parsed_line)
+    # skip erroneous lines
+    if parsed_line[0] not in valid_parameters:
+        continue
+    parsed_lines.append(parsed_line)
+
