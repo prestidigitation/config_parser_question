4c60f41cffb0116eb53a020505911dd1fb7df0ef
Alexander Rowland <rowlanda@protonmail.com>
2020-07-24 20:53:00 -0400

    Put parsed key/value pairs into a hash

--- parser.py
+++ parser.py
@@ -30,3 +30,33 @@ for line in data:
         continue
     parsed_lines.append(parsed_line)
 
+bool_parameters = [
+    'verbose',
+    'test_mode',
+    'debug_mode',
+    'send_notifications'    
+]
+bool_conversion_dict = {
+    'true': True,
+    'false': False,
+    'yes': True,
+    'no': False,
+    'on': True,
+    'off': False
+}
+hashed_data = {}
+for key, value in parsed_lines:
+    if key in bool_parameters:
+        # check if parameter should have a boolean value and try to add it to hash, otherwise skip it
+        if value in bool_conversion_dict:
+            hashed_data[key] = bool_conversion_dict[value]
+        else:
+            continue
+    elif key == 'server_load_alarm':
+        # throws error if float conversion fails
+        hashed_data[key] = float(value)
+    else:
+        # add remaining string-based key/value pairs to hash
+        hashed_data[key] = value
+
+print(hashed_data)
